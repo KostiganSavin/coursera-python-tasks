3d65436aa54666f065f905cfffacee3f2c106457
KostiganSavin <kostigan.savin@gmail.com>
2017-09-13 22:50:51 +0300

    Add writing data to file and reaing data fom file previously writed.

--- key_value/solution.py
+++ key_value/solution.py
@@ -4,17 +4,29 @@ import os
 import tempfile
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--key",)
-# parser.add_argument("--value")
+parser.add_argument("--key")
+parser.add_argument("--value")
 args = parser.parse_args()
 
-storage_filename = os.path.join(tempfile.gettempdir(), "storage.data")
+# storage_filename = os.path.join(tempfile.gettempdir(), "storage.data")
+storage_filename = "storage.data"
 
 print(args.key)
+print(args.value)
 print(storage_filename)
-# with open(storage_filename, "w") as f:
-#    f.write(args.key + "\n")
 
+data = {}
+
+if args.key and args.value:
+    data[args.key] = data.get(args.key, []) + [args.value]
+    print(data)
+    with open(storage_filename, "w+") as f:
+        json.dump(data, f)
+
+if not args.value:
+    with open(storage_filename, "r") as f:
+        data = json.load(f)
+        print(data)
 """
 def main():
     print("Script output")
