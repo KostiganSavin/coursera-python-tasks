c7749fc094baa6a6aa654df8fb36e0f7f4ee764c
Konstantin Savin <kostigan.savin@gmail.com>
2017-09-22 16:46:16 +0300

    Finih with car_reader.

--- car_reader/solution.py
+++ car_reader/solution.py
@@ -29,53 +29,65 @@ class Truck(CarBase):
 
     def __init__(self, brand, photo_file_name, carrying, body_whl):
         super().__init__(brand, photo_file_name, carrying, car_type="truck")
-        body_dimensions = body_whl.split("x")
-        # print(body_dimensions)
-        try:
-            self.body_width = float(body_dimensions[0])
-            self.body_heigth = float(body_dimensions[1])
-            self.body_length = float(body_dimensions[2])
-        except ValueError:
+        if body_whl:
+            try:
+                body_dimensions = body_whl.split("x")
+                self.body_width = float(body_dimensions[0])
+                self.body_height = float(body_dimensions[1])
+                self.body_length = float(body_dimensions[2])
+            except ValueError:
+                raise
+        else:
             self.body_width = 0.0
             self.body_height = 0.0
             self.body_length = 0.0
-        
-    def get_body_volue(self):
+
+
+    def get_body_volume(self):
         return self.body_width * self.body_height * self.body_length  
 
 
 class SpecialMachine(CarBase):
     
     def __init__(self, brand, photo_file_name, carrying, extra):
-        super().__init__(brand, photo_file_name, carrying, car_type="extra")
-        self.extra = extra
-        
+        super().__init__(brand, photo_file_name, carrying, car_type="spec_machine")
+        self.extra = extra      
+
 
 def get_car_list(csv_filename):
     car_list = []
-    with open(csv_filename, "r", encoding="utf-8") as csv_fd:
-        reader = csv.reader(csv_fd, delimiter=";")
-        next(reader)
+    with open(csv_filename, encoding="utf-8") as csv_fd:
+        reader = csv.DictReader(csv_fd, delimiter=";")
         for row in reader:
-            if len(row) != 0:
-                try:
-                    if row[0] == "car":
-                        car = Car(row[1], row[3], row[5], row[2])
-                        car_list.append(car)
-                    elif row[0] == "truck":
-                        truck = Truck(row[1], row[3], row[5], row[4])
-                        car_list.append(truck)
-                    elif row[0] == "spec_machine":
-                        spec_machine = SpecialMachine(row[1], row[3], row[5], row[6])
-                        car_list.append(spec_machine)
-                except IndexError:
-                    continue
-            # print(row)
-    return car_list
+            if not (row["car_type"] and row["brand"] and row["photo_file_name"]
+                    and row["carrying"]):
+                continue
+            try:
+                if row["car_type"] == "car":
+                    car = Car(brand = row["brand"], 
+                            photo_file_name= row["photo_file_name"], 
+                            carrying = row["carrying"], 
+                            passenger_seats_count = row["passenger_seats_count"])
+                if row["car_type"] == "truck":
+                    car = Truck(brand = row["brand"], 
+                                photo_file_name= row["photo_file_name"], 
+                                carrying = row["carrying"],
+                                body_whl = row["body_whl"])
 
+                if row["car_type"] == "spec_machine":
+                    car = SpecialMachine(brand = row["brand"], 
+                                                photo_file_name= row["photo_file_name"],
+                                                carrying = row["carrying"],
+                                                extra = row["extra"])
+                car_list.append(car)
+            except ValueError:
+                continue
+        return car_list
+                
 
 def _main():
     print(get_car_list("coursera_week3_cars.csv"))
+
 
 if __name__ == "__main__":
     _main()
