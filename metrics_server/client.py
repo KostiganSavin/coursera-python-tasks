8e8ab1e199119917d911486ebd448fb9129b67fe
KostiganSavin <kostigan.savin@gmail.com>
2017-10-13 00:05:29 +0300

    Finished server for metrics. Added functions _get, _put

--- metrics_server/client.py
+++ metrics_server/client.py
@@ -73,8 +73,8 @@ class Client:
 
 
 def _main():
-    client = Client("127.0.0.1", 8888, timeout=2)
-    client.put("palm.cpu", 0.5, timestamp=1150864247)
+    client = Client("127.0.0.1", 8888, timeout=5)
+    # client.put("palm.cpu", 0.5, timestamp=1150864247)
     client.put("palm.cpu", 0.5, timestamp=1150864247)
     client.put("palm.cpu", 2.0, timestamp=1150864248)
     client.put("palm.cpu", 0.5, timestamp=1150864248)
@@ -82,7 +82,8 @@ def _main():
     client.put("eardrum.cpu", 3, timestamp=1150864250)
     client.put("eardrum.cpu", 4, timestamp=1150864251)
     client.put("eardrum.memory", 4200000)
-    client.get("palm.cpu")
+    print(client.get("palm.cpu"))
+    print(client.get("*"))
 
     client.close()
 
--- metrics_server/server.py
+++ metrics_server/server.py
@@ -6,6 +6,62 @@ def run_server(host, port):
     pass
 
 
+def _put(payload):
+    global metrics
+    # print("PUT: ", payload)
+    key, value, timestamp = payload.split(" ")
+    
+    if not key in metrics:
+        metrics[key] = []
+        metrics[key].append((int(timestamp), float(value)))
+    else:
+        last_element = metrics[key].pop()
+        # print(last_element)
+        if last_element[0] == int(timestamp):
+            metrics[key].append((int(timestamp), float(value)))
+        else:
+            metrics[key].append(last_element)
+            metrics[key].append((int(timestamp), float(value)))
+    # print(metrics)
+    return "ok\n\n"
+
+
+def _get(key):
+    global metrics
+    print("GET: ", key)
+    resp = "ok\n"
+    if key not in metrics and key !="*":
+        return resp + "\n"
+
+    if key == "*":
+        print(metrics)
+        for item in metrics:
+            for values in metrics[item]:
+                resp += "{0} {1} {2}\n".format(item, values[1], values[0])
+    else:
+        print(metrics[key])
+        for values in metrics[key]:
+            resp += "{0} {1} {2}\n".format(key, values[1], values[0])
+    
+    return resp + "\n"
+
+
+def process_data(data):
+    command, payload = data.split(" ", 1)
+    
+    if not command in commands:
+        return "error\nwrong command\n\n"
+    
+    payload = payload.strip()
+    
+    return commands[command](payload)
+
+
+commands = {"put": _put,
+            "get": _get,
+            }
+
+
 class ClientServerProtocol(asyncio.Protocol):
     def connection_made(self, transport):
         peername = transport.get_extra_info("peername")
@@ -20,7 +76,7 @@ class ClientServerProtocol(asyncio.Protocol):
 loop = asyncio.get_event_loop()
 coro = loop.create_server(
     ClientServerProtocol,
-    '127.0.0.1', 8181
+    '127.0.0.1', 8888
 )
 
 server = loop.run_until_complete(coro)
